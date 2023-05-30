b59bef02f02519140106ec28aa16a2f1d8b06353
☈equired ★ <label.isaiah@gmail.com>
2023-05-30 16:21:31 +0100

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+ALLIGNMENT = "center"
+FONT = ("courier", 20, "normal")
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.pu()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def refresh(self):
+        x_position = self.xcor() + self.x_move
+        y_position = self.ycor() + self.y_move
+        self.goto(x_position, y_position)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_ball(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
